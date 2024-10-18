2698487c0b35f476699baddaef928f1f800cd07a
Антон <yznikfortyni@gmail.com>
2024-10-18 19:46:19 +0300

    Работа над Home

--- TeacherPortal/Program.cs
+++ TeacherPortal/Program.cs
@@ -9,14 +9,15 @@ namespace TeacherPortal
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAuthorizationCore(); // Для Blazor WebAssembly
+
+
 
             builder.Services.AddScoped<Radzen.DialogService>();
             builder.Services.AddScoped<Radzen.NotificationService>();
             builder.Services.AddScoped<Radzen.ContextMenuService>();
             builder.Services.AddScoped<Radzen.TooltipService>();
-            builder.Services.AddScoped<Radzen.ThemeService>(); // Добавляем службу ThemeService
-
-
 
             await builder.Build().RunAsync();
         }
